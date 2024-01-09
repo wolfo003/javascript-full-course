3e3504f2fa4ab2444f5e6f463590f00f0ecbd649
wolfo003 <156049862+wolfo003@users.noreply.github.com>
2024-01-09 07:15:36 +0000

    conversion

---
+++ .devcontainer/javascript/datatypeconversion.js
@@ -0,0 +1,35 @@
+// let score = null; //it gives the number o
+// let valInnumber = Number(score);
+// console.log(typeof valInnumber);
+// console.log(valInnumber);
+
+// ///////////////for undefined//////////////////////////
+
+// let score1 = undefined; //it gives the number o
+// let valInnumber2 = Number(score1);
+// console.log(typeof valInnumber2);
+// console.log(valInnumber2)
+ 
+// /////////////////for boolean//////////////////////////////////////
+
+// let score3 = true;//true = 1 ,  false = 0
+// let valInnumber3 = Number(score3);
+// console.log(typeof valInnumber3);
+// console.log(valInnumber3)
+ 
+// let isLoggedin = "1"
+// let isLoggedinor = Boolean(isLoggedin);
+// console.log(typeof isLoggedinor)
+// console.log(isLoggedinor)
+
+let someNumber = 33
+let stringnumber = String(someNumber);
+console.log(typeof stringnumber)
+console.log(stringnumber)
+
+
+/**
+ if we can give the value 1 then it give us true
+ if we can gives the value 0 then it give us false
+ if we can give the empyt string than 
+ */
