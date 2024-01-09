ddcaaf731f367b92cdd866c6087afa2104bd28e1
wolfo003 <156049862+wolfo003@users.noreply.github.com>
2024-01-09 08:04:22 +0000

    operations

---
+++ .devcontainer/javascript/operation.js
@@ -0,0 +1,32 @@
+// let string = "anuj";
+// let numberstring = Number(string);
+// console.log(typeof numberstring);
+// console.log(numberstring);
+
+// let val = 3;
+// let negval = -val;
+// console.log(negval);
+
+
+// console.log(2+2);
+// console.log(2+3);
+// console.log(2-4);
+// console.log(3**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+//  let str1 = "hello";
+//  let str2 = "anuj";
+//  let str3 = str1+str2;
+//  console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// // console.log("1" + 2+2)
+// console.log(1+2+"2")
+// let gamecounter = 100
+// // gamecounter++; //postfix
+// ++gamecounter //prefix
+// console.log(gamecounter)
+
