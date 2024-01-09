ff1bc04267eed5605b1f418de43b2821d27cbf12
wolfo003 <156049862+wolfo003@users.noreply.github.com>
2024-01-09 10:02:36 +0000

    summary

---
+++ .devcontainer/javascript/summary.js
@@ -0,0 +1,31 @@
+// console.log("hello world");
+
+/**
+ types of datatyes
+ primitive =  there are 7 types of datatypes string , Boolean , null , undefined , symbol , big int , 
+ non-primitive = arrays , objects , functions ,  
+ javascript = Javascript is a dynamic typed
+ */
+// const score = 100;
+// const scoreValue =  100.3
+
+const isloggedIn = false;
+const outsideTemp = null;
+
+// const id = Symbol('123');
+// const anotherid = Symbol('123')
+// console.log(id === anotherid); //value is samed but symbol is diffrent
+
+const bigNumber = 12345678n
+console.log(bigNumber)
+
+
+const hero = ["shaktiman","krish","doga"]
+let myobj = {
+  name:"anuj",
+  age:20,
+}
+const myfunction = function(){
+    console.log("hello world")
+}
+console.log(typeof myfunction);
