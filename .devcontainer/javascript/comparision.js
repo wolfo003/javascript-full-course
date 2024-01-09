a78834867897f43a9d95e67d80a8f9e42c7a3345
wolfo003 <156049862+wolfo003@users.noreply.github.com>
2024-01-09 09:33:07 +0000

    comparisions

---
+++ .devcontainer/javascript/comparision.js
@@ -0,0 +1,17 @@
+// console.log("hello world")
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("3"<1);
+
+// console.log(null>0); //first javascript is convert into zero so the result is false
+// console.log(null == 0); 
+// console.log(null>=0); // and in this null is not convet into zeros so the reason is that it cannot convert into zero 
+
+//==== it is used to check the data type
+console.log("2"===2) //it is used to check both datatypes
